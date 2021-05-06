3502632c1dd8f7b5b0d2b374f7b32cab91ea5bf9
RRRAMOS <dsraquelramos@gmail.com>
2021-05-06 01:04:12 -0300

    projetos criados

---
+++ backend/src/main/java/com/rrramos/controlevendas/ControlevendasApplication.java
@@ -0,0 +1,13 @@
+package com.rrramos.controlevendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlevendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlevendasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/rrramos/controlevendas/ControlevendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rrramos.controlevendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlevendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
